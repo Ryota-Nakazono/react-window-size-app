71d6d18a4cacad1adb3c094386036f99e1ccd907
Ryota-Nakazono <ryota.shunpu.sfz2@gmail.com>
2023-06-21 17:03:38 +0900

    handleRiseize関数の定義位置を変更

--- src/components/WindowSize.js
+++ src/components/WindowSize.js
@@ -4,12 +4,13 @@ export default function WindowSize() {
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
 
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
   useEffect(() => {
     window.alert("マウントされました");
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-      setHeight(window.innerHeight);
-    };
     window.addEventListener("resize", handleResize);
     return () => {
       window.alert("アンマウントされました");
